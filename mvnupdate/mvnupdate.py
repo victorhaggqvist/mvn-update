f09fcc9b3711b5121e1497badd6f7fb5af20146d
Victor Häggqvist <victor@snilius.com>
2015-10-23 03:38:06 +0200

    bump patch

--- mvnupdate/mvnupdate.py
+++ mvnupdate/mvnupdate.py
@@ -10,7 +10,7 @@ from collections import namedtuple
 import logging
 
 __author__ = 'Victor Häggqvist'
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 MetaData = namedtuple('MetaData', ['group', 'artifact', 'version', 'aar'])
 VersionCheck = namedtuple('VersionCheck', ['version', 'metadata'])
